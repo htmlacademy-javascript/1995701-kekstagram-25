e312552c39b22d9ae922ab548977429998318c0b
EugeneSmoth <84969562+EugeneSmoth@users.noreply.github.com>
2022-03-09 16:52:18 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,21 +2,20 @@ const returnNumber = function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min>=max){
-    console.log(-1); }
+    return false};
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-
 console.log(returnNumber (0, 10));
 
-const leightString = function сheckStringLenght (inputText, maxLenght){
-if (inputText.lenght > maxLenght){
+const leightString = function сheckStringLenght ( inputText, maxlenght){
+if(inputText.length > maxlenght){
   return false;
-  console.log('Слишком длинный комментарий');
-} else {
-  return true};
-
 };
+return true;
+};
+console.log(leightString ('комментарииииииииииииииии',40));
 console.log(leightString ('комментарииииииииииииииии',10));
